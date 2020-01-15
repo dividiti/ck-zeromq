053c90a324b9763b8f32f6ab7e0c334e231969e2
Leo Gordon <ens-lg4@users.noreply.github.com>
2020-01-15 12:40:38 +0000

    also print 99% percentile

--- program/image-classification-zpp-hub-loadgen-py/zpp_hub_classify_loadgen.py
+++ program/image-classification-zpp-hub-loadgen-py/zpp_hub_classify_loadgen.py
@@ -255,6 +255,7 @@ def process_latencies(latencies_ns):
     latencies_sorted    = sorted(latencies_ms)
     latencies_p50       = int(latencies_size * 0.5);
     latencies_p90       = int(latencies_size * 0.9);
+    latencies_p99       = int(latencies_size * 0.99);
 
     print("--------------------------------------------------------------------")
     print("|                LATENCIES (in milliseconds and fps)               |")
@@ -264,6 +265,7 @@ def process_latencies(latencies_ns):
     print("Median latency:              {:9.2f} ms   ({:.3f} fps)".format(latencies_sorted[latencies_p50], 1e3/latencies_sorted[latencies_p50]))
     print("Average latency:             {:9.2f} ms   ({:.3f} fps)".format(latencies_avg, 1e3/latencies_avg))
     print("90 percentile latency:       {:9.2f} ms   ({:.3f} fps)".format(latencies_sorted[latencies_p90], 1e3/latencies_sorted[latencies_p90]))
+    print("99 percentile latency:       {:9.2f} ms   ({:.3f} fps)".format(latencies_sorted[latencies_p99], 1e3/latencies_sorted[latencies_p99]))
     print("Max latency:                 {:9.2f} ms   ({:.3f} fps)".format(latencies_sorted[-1], 1e3/latencies_sorted[-1]))
     print("--------------------------------------------------------------------")
 
