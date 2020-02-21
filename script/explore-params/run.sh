d39ee3461a396b044162e9e4d73492651fa167b3
Anton Lokhmotov <anton@dividiti.com>
2020-02-21 21:05:28 +0900

    Pass dataset size to hub. Reduce to 500 by default.

--- script/explore-params/run.sh
+++ script/explore-params/run.sh
@@ -80,7 +80,7 @@ fi
 echo "- mode: ${mode} (${mode_tag})"
 
 if [ "${mode}" = "AccuracyOnly" ]; then
-  dataset_size=50000
+  dataset_size=500 # 50000
   buffer_size=500
 else
   dataset_size=1024
@@ -132,6 +132,7 @@ ck benchmark program:image-classification-zpp-hub-loadgen-py --repetitions=1 \
 --env.CK_LOADGEN_COUNT_OVERRIDE=${count} \
 --env.CK_LOADGEN_MULTISTREAMNESS=${multistreamness} \
 --env.CK_LOADGEN_WARM_UP_SAMPLES=${warm_up_samples} \
+--env.CK_LOADGEN_DATASET_SIZE=${dataset_size} \
 --env.CK_BATCH_SIZE=${batch_size} \
 --env.CK_ZMQ_TRANSFER_MODE=${transfer_mode} \
 --env.CK_FP_MODE=${fp_mode_tag} \
