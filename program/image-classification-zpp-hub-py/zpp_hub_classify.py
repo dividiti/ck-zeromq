dfa80cf69d776b9119f842fcf2d549ab859ede32
Anton Lokhmotov <anton@dividiti.com>
2020-04-23 17:44:51 +0100

    Set real time priority on Windows.

--- program/image-classification-zpp-hub-py/zpp_hub_classify.py
+++ program/image-classification-zpp-hub-py/zpp_hub_classify.py
@@ -17,6 +17,21 @@ except NameError:
     raw_input = input
 
 
+import sys
+try:
+    sys.getwindowsversion()
+except AttributeError:
+    win = False
+else:
+    win = True
+
+if win:
+    import win32api,win32process,win32con
+    pid = win32api.GetCurrentProcessId()
+    handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pid)
+    print("Setting REALTIME_PRIORITY_CLASS on Windows ..."
+    win32process.SetPriorityClass(handle, win32process.REALTIME_PRIORITY_CLASS)
+
 ## Model properties:
 #
 MODEL_PATH              = os.environ['CK_ENV_TENSORRT_MODEL_FILENAME']
