f5c4bbd98fb7b92b7a1de454f01496f663da79c6
Anton Lokhmotov <anton@dividiti.com>
2020-04-28 18:33:00 +0100

    Set HIGH_PRIORITY_CLASS on Windows.

--- program/image-classification-zpp-hub-py/zpp_hub_classify.py
+++ program/image-classification-zpp-hub-py/zpp_hub_classify.py
@@ -29,8 +29,9 @@ if win:
     import win32api,win32process,win32con
     pid = win32api.GetCurrentProcessId()
     handle = win32api.OpenProcess(win32con.PROCESS_ALL_ACCESS, True, pid)
-    print("Setting REALTIME_PRIORITY_CLASS on Windows ...")
-    win32process.SetPriorityClass(handle, win32process.REALTIME_PRIORITY_CLASS)
+    # https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-setpriorityclass
+    print("Setting HIGH_PRIORITY_CLASS on Windows ...")
+    win32process.SetPriorityClass(handle, win32process.HIGH_PRIORITY_CLASS)
 
 ## Model properties:
 #
